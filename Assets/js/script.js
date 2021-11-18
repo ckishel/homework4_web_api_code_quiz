61594af1a51f10bf95906860d7515dff12af5b76
Chris Kishel <ckishel@aptean.com>
2021-11-18 18:20:18 -0500

    started javascript and used console log to test button

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,8 +1,16 @@
-// function to start the game
-functionstartGame(){
+// setting a "constant" var to select my start button from html index
+const startButton = document.getElementById('start-btn')
 
+startButton.addEventListener('click', startGame)
+
+// function to start the game
+function startGame(){
+    console.log('Started')
 }
 // function to start the next question and click on the next button
 function setNextQuestion(){
+
+//function to select the correct answer from list 
+}function selectAnswer(){
 
 }
