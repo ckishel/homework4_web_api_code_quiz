6d2e7ef01e605d3e33b5d496da66297c92323275
Chris Kishel <ckishel@aptean.com>
2021-12-12 19:12:55 -0500

    added the questions to the questions JS file

---
+++ assets/js/questions.js
@@ -0,0 +1,57 @@
+// list all questions, possible choices, and correct answers
+
+var questions = [
+    {
+        title: "Commonly used data types DO NOT include:",
+        choices: [
+          "alerts",
+          "strings",
+          "booleans",
+          "numbers"
+        ],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ____:",
+        choices: [
+          "parentheses",
+          "quotes",
+          "curly brackets",
+          "square brackets"
+        ],
+        answer: "parentheses"
+    },
+    {
+        title: "Arrays in JavaScript can be used to store ____:",
+        choices: [
+          "all of the above",
+          "numbers and strings",
+          "other arrays",
+          "booleans"
+        ],
+        answer: "all of the above"
+    },
+    {
+        title: "String values must be enclosed within ____ when being assigned to variables:",
+        choices: [
+          "quotes",
+          "commas",
+          "curly brackets",
+          "parentheses"
+        ],
+        answer: "quotes"
+    },
+    {
+        title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: [
+          "console.log",
+          "JavaScript",
+          "terminal",
+          "bash for loops"
+        ],
+        answer: "console.log"
+    },
+];
+
+
+
